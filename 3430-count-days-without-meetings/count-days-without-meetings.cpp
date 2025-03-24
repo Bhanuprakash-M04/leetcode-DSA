75a42685ff49d860a860eec0ac8c46d5b96d2982
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-03-24 12:13:53 +0530

    Time: 73 ms (46.26%) | Memory: 126.5 MB (59.18%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -4,33 +4,20 @@ public:
         int n=meetings.size();
         int cnt=0;
         sort(meetings.begin(),meetings.end());
-        for(int i=0;i<n;i++)
-        {
-            cout << meetings[i][0] << " " << meetings[i][1] << endl;
-        }
         int last_meet=meetings[0][1];
         int first_meet=meetings[0][0];
-        for(int i=0;i<n-1;i++)
+        for(int i=1;i<n;i++)
         {
             int st=meetings[i][0];
             int end=meetings[i][1];
-            
-            int next_st=meetings[i+1][0];
-            int next_end=meetings[i+1][1];
-
-           
-            
-            if(next_st>last_meet)
+            if(st>last_meet)
             {
-                cnt+=(next_st-last_meet)-1;
+                cnt+=(st-last_meet)-1;
             }
-            first_meet=min(first_meet,next_st);
-             last_meet=max(last_meet,next_end);
+            first_meet=min(first_meet,st);
+            last_meet=max(last_meet,end);
         }
-        if(last_meet==days && first_meet==1)
-            return cnt;
-        else 
-            return (first_meet-1)+cnt+(days-last_meet);
+        return (first_meet-1)+cnt+(days-last_meet);
         
     }
 };
