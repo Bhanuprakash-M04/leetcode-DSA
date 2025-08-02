8dddf9b1a97aaf1cbaa4eb2d497ac04a5c3733ae
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-08-02 19:37:18 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (12.30%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int cnt=0;
-        string main;
-        string tmp;
+        stack<char>st;
+        string res="";
         for(int i=0;i<s.length();i++){
-            tmp+=s[i];
             if(s[i]=='(')
-                cnt++;
-            else
-                cnt--;
-            if(cnt==0){
-                for(int j=1;j<tmp.length()-1;j++){
-                    main+=tmp[j];
-                }
-                tmp="";
+            {
+                if(!st.empty())
+                    res+='(';
+                st.push('(');
+            }
+            if(s[i]==')'){
+                st.pop();
+                if(!st.empty())
+                    res+=')';
             }
         }
-        return main;
+        return res;
     }
 };
