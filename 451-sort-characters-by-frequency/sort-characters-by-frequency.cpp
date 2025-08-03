3676c9e71b989f3ff20a6aed415d2b7dce564aa2
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-08-03 11:19:09 +0530

    Time: 6 ms (24.62%) | Memory: 13.6 MB (11.21%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -5,15 +5,19 @@ public:
         for(char ch:s){
             mymap[ch]++;
         }
-        priority_queue<pair<int,char>>pq;
+        int max_freq=0;                         //unordered_map+ bucket sort
         for(auto [ch,cnt]:mymap){
-            pq.push({cnt,ch});
-        }                                       //hashmap+priority_queue
-        string ans="";
-        while(!pq.empty()){
-            auto [ch,cnt]=pq.top();
-            pq.pop();
-            ans.append(ch,cnt);
+            max_freq=max(max_freq,cnt);
+        }
+        vector<vector<char>>bucket(max_freq+1);
+        for(auto [ch,cnt]:mymap){
+            bucket[cnt].push_back(ch);
+        }
+        string ans;
+        for(int i=max_freq;i>=1;i--){
+            for(char ch:bucket[i]){
+                ans.append(i,ch);
+            }
         }
         return ans;
     }
