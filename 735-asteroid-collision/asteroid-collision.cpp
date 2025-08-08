59b8aea48fc21d67a07d1c138e4a9860a2786b91
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-08-08 12:15:53 +0530

    Time: 3 ms (30.14%) | Memory: 22.1 MB (41.98%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -2,54 +2,33 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int>st;
-        vector<int>ans;
-        int n=asteroids.size();
-        st.push(asteroids[0]);
-        for(int i=1;i<n;i++){
-            if(asteroids[i]>0){
-                st.push(asteroids[i]);
-            }
-            else if(st.empty()){
-                st.push(asteroids[i]);
-            }
-            else if(st.top()>0 && asteroids[i]<0){
-                int res=asteroids[i];
-                while(!st.empty()){
-                    int x=st.top();
-                    if(abs(x)==abs(res))
+        for(auto i:asteroids){
+            if(i>0 || st.empty())
+                st.push(i);
+            else{
+                if(!st.empty() && st.top()<0 && i<0)
+                    st.push(i);
+                while(!st.empty() && st.top()>0 && abs(st.top())<=abs(i))
+                {
+                    if(abs(st.top())==abs(i))
                     {
                         st.pop();
                         break;
                     }
                     else{
-                        int val=max(abs(x),abs(res));
                         st.pop();
-                        if(val==abs(res)){
-                            val=-val;
-                            res=val;
-                            if(!st.empty() && st.top()<0)
-                            {
-                                st.push(res);
-                                break; 
-                            }
-                            else if(st.empty())
-                            {
-                                st.push(res);
-                                break;
-                            }
-                        }
-                        else{
-                            st.push(val);
+                        if(st.empty() || st.top()<0)
+                        {
+                            st.push(i);
                             break;
-                        }   
-                    } 
+                        }
+                    }
                 }
             }
-            else if(st.top()<0 && asteroids[i]<0)
-                st.push(asteroids[i]);
-    
         }
-        while(!st.empty()){
+        vector<int>ans;
+        while(!st.empty())
+        {
             ans.push_back(st.top());
             st.pop();
         }
