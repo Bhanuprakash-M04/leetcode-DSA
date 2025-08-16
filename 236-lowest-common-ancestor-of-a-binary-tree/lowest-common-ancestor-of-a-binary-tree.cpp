b32ecd0b92f755002268b6352edf9eea27ffe924
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-08-16 21:19:07 +0530

    Time: 4 ms (98.28%) | Memory: 17.4 MB (41.22%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,30 +9,17 @@
  */
 class Solution {
 public:
-    bool findPath(TreeNode* root,TreeNode* node,vector<TreeNode*>& res){
-        if(root==NULL)
-            return false;
-        res.push_back(root);
-        if(root->val==node->val)
-            return true;
-        if(findPath(root->left,node,res) || findPath(root->right,node,res))
-            return true;
-        res.pop_back();
-        return false;
-    }
+    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>first;
-        vector<TreeNode*>second;
-        findPath(root,p,first);
-        findPath(root,q,second);
-        int n=min(first.size(),second.size());
-        TreeNode* ans=NULL;
-        for(int i=0;i<n;i++)
-        {
-            if(first[i]->val==second[i]->val)
-                ans=first[i];
-        }
-        return ans;
+       if(root==NULL || root==p || root==q)
+            return root;
+        TreeNode* Left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* Right=lowestCommonAncestor(root->right,p,q);
+        if(Left==NULL)
+            return Right;
+        if(Right==NULL)
+            return  Left;
+        return root;
     }
 
 };
