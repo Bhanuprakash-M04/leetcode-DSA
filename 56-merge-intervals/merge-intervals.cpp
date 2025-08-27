6f6b7d5d15c523bcc84a35670896a3ec27440cb6
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-08-27 14:58:35 +0530

    Time: 2 ms (93.28%) | Memory: 23.7 MB (94.11%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,21 +5,16 @@ public:
         sort(interval.begin(),interval.end());
         int n=interval.size();
         vector<int>temp(2);
-        int first=interval[0][0];
-        int second=interval[0][1];
-        temp[0]=first;
-        temp[1]=second;
+        temp[0]=interval[0][0];
+        temp[1]=interval[0][1];
         for(int i=1;i<n;i++){
-            if(interval[i][0]<=second){
-                second=max(second,interval[i][1]);
-                temp[1]=second;
+            if(interval[i][0]<=temp[1]){
+               temp[1]= max(temp[1],interval[i][1]);
             }
             else{
                 ans.push_back(temp);
-                first=interval[i][0];
-                second=interval[i][1];
-                temp[0]=first;
-                temp[1]=second;
+                temp[0]=interval[i][0];
+                temp[1]=interval[i][1];
             }
         }
         ans.push_back(temp);
