42347a53bfcc9c6166dba948b53807dec7d54928
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-09-16 11:36:08 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.90%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -6,18 +6,17 @@ public:
         int high=n-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]==target)
+            if(nums[mid]==target){
                 return mid;
-            else if(nums[low]<=nums[mid])
-            {
-                if(target>=nums[low] && target<=nums[mid])
+            }
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid])
                     high=mid-1;
                 else
                     low=mid+1;
             }
-            else
-            {
-                if(target<=nums[high] && target>=nums[mid])
+            else if(nums[mid]<=nums[high]){
+                if(nums[mid]<=target && target<=nums[high])
                     low=mid+1;
                 else
                     high=mid-1;
