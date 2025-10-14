ffab6eb3c7b448ab92d9a470530593098f979cde
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-10-14 10:53:20 +0530

    Time: 20 ms (7.02%) | Memory: 27.9 MB (5.18%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            while(nums[i]<n && nums[i]!=nums[nums[i]])
-                swap(nums[i],nums[nums[i]]);
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     while(nums[i]<n && nums[i]!=nums[nums[i]])
+        //         swap(nums[i],nums[nums[i]]);
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]!=i)
+        //         return i;
+        // }
+        // return n;
+
+
+        
+        int maxi=*max_element(nums.begin(),nums.end());
+        unordered_set<int>myset;
+        for(auto num:nums){
+            myset.insert(num);
         }
-        for(int i=0;i<n;i++){
-            if(nums[i]!=i)
+        for(int i=0;i<=maxi;i++){
+            if(myset.find(i)==myset.end()){
                 return i;
+            }
         }
-        return n;
+        return nums.size();
     }
 };
