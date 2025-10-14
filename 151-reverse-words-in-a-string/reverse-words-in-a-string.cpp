43e5ef8168066892a409637dadf8a09868bae4e2
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-10-14 20:05:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (25.32%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string>st;
-        string tmp="";
-        for(int i=0;i<s.length();i++){
-            if(s[i]!=' '){
-                tmp+=s[i];
-            }
-            else{
-                if(tmp!="")
-                    st.push(tmp);
-                tmp="";
-            }
+        // stack<string>st;
+        // string tmp="";
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]!=' '){
+        //         tmp+=s[i];
+        //     else{
+        //         if(tmp!="")
+        //             st.push(tmp);
+        //         tmp="";
+        //     }
+        // }
+        // if(tmp!="")
+        //     st.push(tmp);
+        // string ans="";
+        // while(!st.empty()){
+        //     if(ans!="")
+        //         ans=ans+" "+st.top();
+        //     else   
+        //         ans+=st.top();
+        //     st.pop();
+        // }
+        // return ans;
+
+        stringstream ss(s);
+        string word;
+        vector<string>words;
+        while(ss >> word){
+            words.push_back(word);
         }
-        if(tmp!="")
-            st.push(tmp);
-        string ans="";
-        while(!st.empty()){
-            if(ans!="")
-                ans=ans+" "+st.top();
-            else   
-                ans+=st.top();
-            st.pop();
+        reverse(words.begin(),words.end());
+        string res;
+        int n=words.size();
+        for(int i=0;i<n;i++){
+            res+=words[i];
+            if(i!=n-1)
+                res+=" ";
         }
-        return ans;
+        return res;
     }
 };
