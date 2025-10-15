7c690734d9eda2127e54f3569f29da937854cfe7
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-10-15 10:59:12 +0530

    Time: 4 ms (88.37%) | Memory: 23 MB (43.64%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -11,31 +11,22 @@ public:
             else
                 cnt+=((piles[i]/mid));
         }
-        if(cnt>h){
+        if(cnt>h)
             return false;
-        }
         return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi=INT_MIN;
-        int n=piles.size();
-        for(int i=0;i<n;i++){
-            maxi=max(maxi,piles[i]);
-        }
         int low=1;
-        int high=maxi;
+        int high=*max_element(piles.begin(),piles.end());
         int ans=INT_MAX;
         while(low<=high){
             int mid=(low+high)/2;
-            bool value=check(piles,h,mid);
-            if(value){
+            if(check(piles,h,mid)){
                 ans=min(ans,mid);
                 high=mid-1;
             }
             else
-            {
                 low=mid+1;
-            }
         }
         return ans;
     }
