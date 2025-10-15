6e57a879d0200dd0977c1555c44feb04bcc26295
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-10-15 18:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 94.9 MB (93.72%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -5,29 +5,22 @@ public:
         int m=nums2.size();
         int sec_ind=(n+m)/2;
         int first_ind=sec_ind-1;
-        int first_ele=-1;
-        int sec_ele=-1;
-        int p1=0;
-        int p2=0;
-        int cnt=0;
+        int first_ele=-1,sec_ele=-1;
+        int p1=0, p2=0,cnt=0;
         while(p1<n && p2<m){
             if(nums1[p1]<nums2[p2]){
-                if(cnt==first_ind){
+                if(cnt==first_ind)
                     first_ele=nums1[p1];
-                }
-                if(cnt==sec_ind){
+                if(cnt==sec_ind)
                     sec_ele=nums1[p1];
-                }
                 p1++;
                 cnt++;
             }
             else{
-                if(cnt==first_ind){
+                if(cnt==first_ind)
                     first_ele=nums2[p2];
-                }
-                if(cnt==sec_ind){
+                if(cnt==sec_ind)
                     sec_ele=nums2[p2];
-                }
                 p2++;
                 cnt++;
             }
@@ -35,22 +28,18 @@ public:
                 break;
         }
         while(p1<n && sec_ele==-1){
-            if(cnt==first_ind){
+            if(cnt==first_ind)
                 first_ele=nums1[p1];
-            }
-            if(cnt==sec_ind){
+            if(cnt==sec_ind)
                 sec_ele=nums1[p1];
-            }
             p1++;
             cnt++;
         }
         while(p2<m && sec_ele==-1){
-            if(cnt==first_ind){
+            if(cnt==first_ind)
                 first_ele=nums2[p2];
-            }
-            if(cnt==sec_ind){
+            if(cnt==sec_ind)
                 sec_ele=nums2[p2];
-            }
             p2++;
             cnt++;
         }
