31721c6beb1c3ba047d537136bbc5f59c92130c2
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-10-15 18:41:25 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (93.72%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,22 +3,57 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size();
         int m=nums2.size();
-        vector<int>temp(n+m);
+        int sec_ind=(n+m)/2;
+        int first_ind=sec_ind-1;
+        int first_ele=-1;
+        int sec_ele=-1;
         int p1=0;
         int p2=0;
-        int k=0;
+        int cnt=0;
         while(p1<n && p2<m){
-            if(nums1[p1]<nums2[p2])
-                temp[k++]=nums1[p1++];
-            else
-                temp[k++]=nums2[p2++];
+            if(nums1[p1]<nums2[p2]){
+                if(cnt==first_ind){
+                    first_ele=nums1[p1];
+                }
+                if(cnt==sec_ind){
+                    sec_ele=nums1[p1];
+                }
+                p1++;
+                cnt++;
+            }
+            else{
+                if(cnt==first_ind){
+                    first_ele=nums2[p2];
+                }
+                if(cnt==sec_ind){
+                    sec_ele=nums2[p2];
+                }
+                p2++;
+                cnt++;
+            }
+            if(sec_ele!=-1)
+                break;
         }
-        while(p1<n){
-            temp[k++]=nums1[p1++];
+        while(p1<n && sec_ele==-1){
+            if(cnt==first_ind){
+                first_ele=nums1[p1];
+            }
+            if(cnt==sec_ind){
+                sec_ele=nums1[p1];
+            }
+            p1++;
+            cnt++;
         }
-        while(p2<m){
-            temp[k++]=nums2[p2++];
+        while(p2<m && sec_ele==-1){
+            if(cnt==first_ind){
+                first_ele=nums2[p2];
+            }
+            if(cnt==sec_ind){
+                sec_ele=nums2[p2];
+            }
+            p2++;
+            cnt++;
         }
-        return (n+m)%2==0 ? (((double)temp[(n+m)/2]+(double)temp[(n+m)/2-1])/2) : (double)temp[(n+m)/2];
+        return (n+m)%2==0 ? ((double)first_ele+(double)sec_ele)/2 : (double)sec_ele;
     }
 };
