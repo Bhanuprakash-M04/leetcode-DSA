edb1a9df1e37634872c4aaee05b0a28d514b4a9d
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-10-15 10:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.71%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -14,10 +14,10 @@ public:
             int mid=(low+high)/2;
             if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
                 return mid;
-            else if(nums[mid]>nums[mid-1])
-                low=mid+1;
-            else
+            else if(nums[mid]<nums[mid-1])
                 high=mid-1;
+            else
+                low=mid+1;
         }
         return -1;
     }
