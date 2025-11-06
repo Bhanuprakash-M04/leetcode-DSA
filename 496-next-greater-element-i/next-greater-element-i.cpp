a85f2022ce0689dcc094612019e2e9c1d53911b7
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-11-06 23:10:51 +0530

    Time: 2 ms (48.99%) | Memory: 12.8 MB (37.12%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -7,22 +7,16 @@ public:
         stack<int>st;
         unordered_map<int,int>mymap;
         for(int i=m-1;i>=0;i--){
+            while(!st.empty() && nums2[i]>=st.top())
+                st.pop();
             if(st.empty()){
                 mymap[nums2[i]]=-1;
                 st.push(nums2[i]);
             }
             else{
-                while(!st.empty() && nums2[i]>=st.top())
-                    st.pop();
-                if(st.empty()){
-                    mymap[nums2[i]]=-1;
-                    st.push(nums2[i]);
-                }
-                else{
-                    mymap[nums2[i]]=st.top();
-                    st.push(nums2[i]);
-                }
-            }  
+                mymap[nums2[i]]=st.top();
+                st.push(nums2[i]);
+            }
         }
         for(int num:nums1){
              ans.push_back(mymap[num]);
