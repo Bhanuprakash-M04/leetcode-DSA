ffb8bc04b1603e4fce1985657708d7916a7929e0
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-11-06 19:24:26 +0530

    Time: 95 ms (8.58%) | Memory: 29.2 MB (11.78%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -5,17 +5,15 @@ public:
         stack<int>st;
         for(int i=0;i<n;i++){
             int j=i+1;
-            while(j%n<n){
-                if(i==j%n){
-                    st.push(-1);
-                    break;
-                }
+            while(i!=j%n){
                 if(nums[i]<nums[j%n]){
                     st.push(nums[j%n]);
                     break;
                 }
                 j++;
             }
+            if(i==j%n)
+                st.push(-1);
         }
         vector<int>ans;
         while(!st.empty()){
