fa0efe8aee3f052a052593dcdb80403e9d65e6e9
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-12-18 21:22:24 +0530

    Time: 1 ms (15.56%) | Memory: 8.4 MB (24.46%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-    bool rotateString(string s, string goal) {
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            string st=rotate(s,i);
-            if(st==goal)
+    // bool rotateString(string s, string goal) {
+    //     int n=s.length();
+    //     for(int i=0;i<n;i++){
+    //         string st=rotate(s,i);
+    //         if(st==goal)
+    //             return true;
+    //     }
+    //     return false;
+    // }
+    // string rotate(string str,int val){
+    //     string ans=str;
+    //     int n=str.length();
+    //     for(int i=0;i<n;i++){
+    //         ans[i]=str[(i+val)%n];
+    //     }
+    //     return ans;
+    // }
+    bool rotateString(string s, string goal){
+        if(s.size()!=goal.size())
+            return false;
+        string full=s+s;
+        int n=full.size();
+        int m=goal.size();
+        for(int i=0;i<n-m;i++){
+            string sub_str=full.substr(i,m);
+            if(sub_str==goal)
                 return true;
         }
         return false;
     }
-    string rotate(string str,int val){
-        string ans=str;
-        int n=str.length();
-        for(int i=0;i<n;i++){
-            ans[i]=str[(i+val)%n];
-        }
-        return ans;
-    }
+
 };
