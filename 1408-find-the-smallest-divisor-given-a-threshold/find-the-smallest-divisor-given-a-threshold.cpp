4c723ba40854ea8d11d5ab48f02c8daa8a3fc224
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-12-18 12:32:14 +0530

    Time: 4 ms (85.32%) | Memory: 26.2 MB (46.88%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -14,18 +14,18 @@ public:
         return sum;
     }
     int smallestDivisor(vector<int>& nums, int threshold) {
+        if(threshold<nums.size())
+            return -1;
         int low=1;
         int high=*max_element(nums.begin(),nums.end());
-        int ans=-1;
         while(low<=high){
             int mid=(low+high)/2;
             if(check(nums,mid)<=threshold){
-                ans=mid;
                 high=mid-1;
             }
             else
                 low=mid+1;
         }
-        return ans;
+        return low;
     }
 };
