ba6aa9a0b4bc52a7e78b64ace43542ded28da97b
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-12-27 12:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (61.56%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int n=nums.size();
-        unordered_map<int,int>mymap;
+        return subarrayLessEquals(nums,goal)-subarrayLessEquals(nums,goal-1);
+    }
+    int subarrayLessEquals(vector<int>& nums,int goal){
+        int left=0;
+        int ans=0;
         int sum=0;
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            if(sum==goal)
-                cnt++;
-            if(mymap.find(sum-goal)!=mymap.end())
-                cnt+=mymap[sum-goal];
-            mymap[sum]++;
+        int n=nums.size();
+        for(int right=0;right<n;right++){
+            sum+=nums[right];
+            while(sum>goal && left<=right){
+                sum-=nums[left];
+                left++;
+            }
+            ans+=(right-left+1);
         }
-        return cnt;
+        return ans;
     }
 };
+
+
+
+// TC:O(n)
+// SC:O(N)
+// int n=nums.size();
+//         unordered_map<int,int>mymap;
+//         int sum=0;
+//         int cnt=0;
+//         for(int i=0;i<n;i++){
+//             sum+=nums[i];
+//             if(sum==goal)
+//                 cnt++;
+//             if(mymap.find(sum-goal)!=mymap.end())
+//                 cnt+=mymap[sum-goal];
+//             mymap[sum]++;
+//         }
+//         return cnt;
