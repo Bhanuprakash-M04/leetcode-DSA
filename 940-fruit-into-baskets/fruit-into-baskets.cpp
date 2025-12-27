2cf58bba720abed39aba7b64efb9fd69713bae83
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-12-27 10:40:25 +0530

    Time: 40 ms (62.40%) | Memory: 85 MB (29.49%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -7,13 +7,14 @@ public:
         int ans=0;
         for(int right=0;right<n;right++){
             mymap[fruits[right]]++;
-            while(mymap.size()>2){
+            if(mymap.size()>2){
                 mymap[fruits[left]]--;
                 if(mymap[fruits[left]]==0)
                     mymap.erase(fruits[left]);
                 left++;
             }
-            ans=max(ans,right-left+1);
+            if(mymap.size()<=2)
+                ans=max(ans,right-left+1);
         }
         return ans;
     }
