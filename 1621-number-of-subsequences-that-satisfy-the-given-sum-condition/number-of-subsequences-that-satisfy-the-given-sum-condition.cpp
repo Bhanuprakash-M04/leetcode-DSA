5d992573cf0c389cebec01e6a7b7ae59e82f1b00
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2026-01-07 18:19:45 +0530

    Time: 31 ms (55.95%) | Memory: 54.2 MB (64.60%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     int m=1e9+7;
     int numSubseq(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int>power(n);
+        power[0]=1;
         sort(nums.begin(),nums.end());
-        int p1=0;
-        int p2=nums.size()-1;
-        int cnt=0;
-        vector<int>pow2(nums.size());
-        pow2[0]=1;
-        for(int i=1;i<nums.size();i++){
-            pow2[i]=(pow2[i-1]*2)%m;
+        for(int i=1;i<n;i++){
+            power[i]=(power[i-1]*2)%m;
         }
-        while(p1<=p2){
-            if(nums[p1]+nums[p2]<=target){
-                long long x=p2-p1;
-                cnt=(cnt+pow2[x])%m;
-                p1++;
+        int left=0;
+        int right=n-1;
+        int cnt=0;
+        while(left<=right){
+            if(nums[left]+nums[right]<=target){
+                cnt=(cnt+power[right-left])%m;
+                left++;
             }
-            else{
-                p2--;
-            }
+            else
+                right--;
         }
         return cnt;
     }
